9542ef7e62102d3d9d8c976583b4aee9f94c2ffc
asilJPG <asilchik18@gmail.com>
2026-04-08 00:12:56 +0500

    tg fix

--- next.config.js
+++ next.config.js
@@ -10,6 +10,24 @@ const nextConfig = {
       { protocol: "https", hostname: "maps.gstatic.com" },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PATCH,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization, X-Telegram-Init-Data",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
